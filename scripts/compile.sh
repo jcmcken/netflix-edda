af3520f2bf3f24e4a51b75653423e6c1279ac9d2
Jon McKenzie <jcmcken@gmail.com>
2014-10-13 23:35:57 -0400

    Just run the distributed gradlew wrapper for consistency

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,8 +5,8 @@ ROOT_DIR=$(readlink -f $(dirname $0)/..)
 source $ROOT_DIR/scripts/config.sh
 source $APP_HOME/etc/edda-env.sh
 
-pushd $APP_HOME/$SRC_DIR_NAME
-./gradlew --no-daemon build
+pushd $APP_HOME
+./bin/gradlew build
 popd
 
 exit 0
